ee51cadcb51a718d49597808b9583effdce19e68
shotastage <shota.shimazu@magicalsoft.app>
2025-03-05 15:25:53 +0900

    feat: add password strength evaluation using zxcvbn algorithm

--- Sources/SecurityKit/PasswordUtils/PasswdStrength.swift
+++ Sources/SecurityKit/PasswordUtils/PasswdStrength.swift
@@ -1,12 +1,11 @@
 //
 //  PasswdStrength.swift
-//  
+//
 //
 //  Created by Shota Shimazu on 2022/07/04.
 //
 
 import Foundation
-
 
 public typealias PasswdStrength = Int
 
@@ -28,5 +27,17 @@ open class Zxcvbn {
     init(matcher: String, scorer: String) {
         self.matcher = matcher
         self.scorer = scorer
+    }
+
+    public func evaluate(password: String) -> PasswdResult {
+        // Here we implement the zxcvbn algorithm
+        // Dummy implementation
+        if password.count < 8 {
+            return .weak
+        } else if password.count < 12 {
+            return .medium
+        } else {
+            return .strong
+        }
     }
 }
